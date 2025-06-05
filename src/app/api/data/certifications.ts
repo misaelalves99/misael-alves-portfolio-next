c25e2027867f35c6eb2a2f2a18f7f9fb30b22dfa
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-06-05 15:26:51 -0300

    Adicionando Certificados

--- src/app/api/data/certifications.ts
+++ src/app/api/data/certifications.ts
@@ -31,6 +31,48 @@ export const cardCertification = [
     desc: "React do Zero a Maestria",
     cert: "/assets/certificado5.pdf",
   },
+  {
+    id: 6,
+    imgcert: "/assets/img-certificado6.png",
+    desc: "React JS com TypeScript",
+    cert: "/assets/certificado6.pdf",
+  },
+  {
+    id: 7,
+    imgcert: "/assets/img-certificado7.png",
+    desc: "Next.js do Zero ao Avançado",
+    cert: "/assets/certificado7.pdf",
+  },
+  {
+    id: 8,
+    imgcert: "/assets/img-certificado8.png",
+    desc: "TypeScript do básico ao avançado",
+    cert: "/assets/certificado8.pdf",
+  },
+    {
+    id: 9,
+    imgcert: "/assets/img-certificado9.png",
+    desc: "JavaScript do básico ao avançado",
+    cert: "/assets/certificado9.pdf",
+  },
+  {
+    id: 10,
+    imgcert: "/assets/img-certificado10.png",
+    desc: "Node.js do Zero a Maestria",
+    cert: "/assets/certificado10.pdf",
+  },
+  {
+    id: 11,
+    imgcert: "/assets/img-certificado11.png",
+    desc: "SQL do básico ao avançado",
+    cert: "/assets/certificado11.pdf",
+  },
+  {
+    id: 12,
+    imgcert: "/assets/img-certificado12.png",
+    desc: "Git e GitHub do básico ao avançado",
+    cert: "/assets/certificado12.pdf",
+  },
 ];
 
 // Função Fake de API
